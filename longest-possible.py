548525c8e5254ecb5f7ec1f75205a81451f5185b
Nathan Brown <scottynate05@yahoo.com>
2020-11-01 22:06:28 -0800

    longest-possible

---
+++ longest-possible.py
@@ -0,0 +1,14 @@
+# Given two strings that include only lowercase alpha characters, str_1 and str_2, write a function that returns a new sorted string that contains any character (only once) that appeared in str_1 or str_2.
+
+# Examples:
+
+# csLongestPossible("aabbbcccdef", "xxyyzzz") -> "abcdefxyz"
+# csLongestPossible("abc", "abc") -> "abc"
+# [execution time limit] 4 seconds (py3)
+
+# [input] string str_1
+
+# [input] string str_2
+
+# [output] string
+
