070ed50e8c3cc93cba05ebcc4f5fa8b31016aab9
Nathan Brown <scottynate05@yahoo.com>
2020-11-01 22:58:42 -0800

    longest-possible complete

--- longest-possible.py
+++ longest-possible.py
@@ -12,3 +12,23 @@
 
 # [output] string
 
+def csLongestPossible(str_1, str_2):
+    # result = []
+    # if(len(str_1) < len(str_2)):
+    #     for i in str_1:
+    #         if(i in str_2):
+    #             result.append(i)
+    # else:
+    #     for i in str_2:
+    #         if(i in str_1):
+    #             result.append(i)
+    # return ''.join(dict.fromkeys(result))
+    new_str = str_1 + str_2
+    # return ''.join(OrderedDict.fromkeys(new_str))
+    t = ''
+    for i in new_str:
+        if(i in t):
+            pass
+        else:
+            t = t + i
+    return ''.join(sorted(t))
